e4138464d98e73c58ff928f20529d3b497efa49b
Matthew Bryce <mattbryce93@outlook.com>
2018-07-12 13:57:28 +0100

    created Restaurant constructors

--- java_project_restaurant/src/main/java/models/Restaurant.java
+++ java_project_restaurant/src/main/java/models/Restaurant.java
@@ -1,4 +1,51 @@
 package models;
 
+import com.sun.tools.javah.Gen;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "restaurants")
 public class Restaurant {
+
+    private int id;
+    private String name;
+    private List<Booking> bookings;
+
+    public Restaurant() {
+    }
+
+    public Restaurant(String name, List<Booking> bookings) {
+        this.name = name;
+        this.bookings = bookings;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "restaurant_name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "restaurant", fetch = FetchType.LAZY)
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
