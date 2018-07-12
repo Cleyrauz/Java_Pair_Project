480ccb833a76c52eff66ed7c8c8e13888a75a556
Matthew Bryce <mattbryce93@outlook.com>
2018-07-12 22:55:43 +0100

    Added Classes working with DB to Runner File

--- java_project_restaurant/src/main/java/Runner.java
+++ java_project_restaurant/src/main/java/Runner.java
@@ -5,7 +5,15 @@ import java.util.ArrayList;
 
 public class Runner {
     public static void main(String[] args) {
+        DBHelper.deleteAll(Restaurant.class);
+
         Restaurant restaurant = new Restaurant("Del Arepas");
         DBHelper.save(restaurant);
+
+        Customer customer = new Customer("Matthew", "Bryce", 100.50);
+        DBHelper.save(customer);
+
+        RestaurantTable restaurantTable = new RestaurantTable(1, 6, restaurant);
+        DBHelper.save(restaurantTable);
     }
 }
