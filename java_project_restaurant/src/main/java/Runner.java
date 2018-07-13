0c05699ed70a783cc45bf17e6286f321b07aa7cc
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-07-13 11:48:48 +0100

    Added seeds

--- java_project_restaurant/src/main/java/Runner.java
+++ java_project_restaurant/src/main/java/Runner.java
@@ -5,59 +5,59 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Runner {
-    public static void main(String[] args) {
-
-        DBHelper.deleteAll(Item.class);
-        DBHelper.deleteAll(Bill.class);
-        DBHelper.deleteAll(Customer.class);
-        DBHelper.deleteAll(Booking.class);
-        DBHelper.deleteAll(RestaurantTable.class);
-        DBHelper.deleteAll(Restaurant.class);
-
-
-
-        Restaurant restaurant = new Restaurant("Del Arepas");
-        DBHelper.save(restaurant);
-
-        Customer customer = new Customer("Matthew", "Bryce", 100.50);
-        DBHelper.save(customer);
-
-        RestaurantTable restaurantTable = new RestaurantTable(1, 6, restaurant);
-        DBHelper.save(restaurantTable);
-
-        Item item = new Item(ItemType.FAJITAS);
-        DBHelper.save(item);
-
-        Date date = new Date(2018, 10, 2, 17, 10);
-
-        Booking booking = new Booking(restaurant, restaurantTable, date, 120);
-        DBHelper.save(booking);
-
-        Bill bill = new Bill(booking);
-        DBHelper.save(bill);
-
-        bill.addItem(item);
-        DBHelper.save(bill);
-
-        item.setBill(bill);
-        DBHelper.save(item);
-
-        restaurant.addBooking(booking);
-        DBHelper.save(restaurant);
-
-        restaurant.addRestaurantTable(restaurantTable);
-        DBHelper.save(restaurant);
-
-        customer.addBooking(booking);
-        DBHelper.save(customer);
-
-        restaurantTable.addBoooking(booking);
-        DBHelper.save(restaurantTable);
-
-        booking.setBill(bill);
-        DBHelper.save(booking);
-
-        booking.addCustomer(customer);
-        DBHelper.save(booking);
+//    public static void main(String[] args) {
+//
+//        DBHelper.deleteAll(Item.class);
+//        DBHelper.deleteAll(Bill.class);
+//        DBHelper.deleteAll(Customer.class);
+//        DBHelper.deleteAll(Booking.class);
+//        DBHelper.deleteAll(RestaurantTable.class);
+//        DBHelper.deleteAll(Restaurant.class);
+//
+//
+//
+//        Restaurant restaurant = new Restaurant("Del Arepas");
+//        DBHelper.save(restaurant);
+//
+//        Customer customer = new Customer("Matthew", "Bryce", 100.50);
+//        DBHelper.save(customer);
+//
+//        RestaurantTable restaurantTable = new RestaurantTable(1, 6, restaurant);
+//        DBHelper.save(restaurantTable);
+//
+//        Item item = new Item(ItemType.FAJITAS);
+//        DBHelper.save(item);
+//
+//        Date date = new Date(2018, 10, 2, 17, 10);
+//
+//        Booking booking = new Booking(restaurant, restaurantTable, date, 120);
+//        DBHelper.save(booking);
+//
+//        Bill bill = new Bill(booking);
+//        DBHelper.save(bill);
+//
+//        bill.addItem(item);
+//        DBHelper.save(bill);
+//
+//        item.setBill(bill);
+//        DBHelper.save(item);
+//
+//        restaurant.addBooking(booking);
+//        DBHelper.save(restaurant);
+//
+//        restaurant.addRestaurantTable(restaurantTable);
+//        DBHelper.save(restaurant);
+//
+//        customer.addBooking(booking);
+//        DBHelper.save(customer);
+//
+//        restaurantTable.addBoooking(booking);
+//        DBHelper.save(restaurantTable);
+//
+//        booking.setBill(bill);
+//        DBHelper.save(booking);
+//
+//        booking.addCustomer(customer);
+//        DBHelper.save(booking);
     }
-}
+
