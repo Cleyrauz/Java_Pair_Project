3d0e4d4c9755ed9198039b64ffae6e6f1e829f6c
Matthew Bryce <mattbryce93@outlook.com>
2018-07-13 13:54:09 +0100

    Added Tables Controller

---
+++ java_project_restaurant/src/main/java/controllers/RestaurantTableController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import models.RestaurantTable;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+public class RestaurantTableController {
+
+    public RestaurantTableController(){this.setupEndPoints();}
+
+    public void setupEndPoints(){
+        get("/tables", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            List<RestaurantTable> tables = DBHelper.getAll(RestaurantTable.class);
+            model.put("template", "templates/tables/index.vtl");
+            model.put("tables", tables);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/tables/new", (req,res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/tables/new.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/tables/:num", (req,res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            RestaurantTable table = DBHelper.find(RestaurantTable.class, Integer.parseInt(req.params(":num")));
+            model.put("template", "templates/tables/show.vtl");
+            model.put("table", table);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/tables/:num/edit", (req,res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            RestaurantTable table = DBHelper.find(RestaurantTable.class, Integer.parseInt(req.params(":num")));
+            model.put("template", "templates/tables/edit.vtl");
+            model.put("table", table);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+    }
+}
